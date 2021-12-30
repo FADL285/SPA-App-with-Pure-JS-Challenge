e4b691b1d52024d5419e00923e5582c4572dcba5
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2021-12-30 22:30:06 +0200

    added Vote up and down on each video request

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ const addCardToView = (...vidReq) => {
   const containerFragment = document.createDocumentFragment();
   vidReq.forEach((el) => {
     const cardEl = document.importNode(videoRequestCardTemplate.content, true);
+    cardEl.querySelector('.card').id = `id_${el._id}285`;
     cardEl.querySelector('.card__title').textContent = el.topic_title;
     cardEl.querySelector('.card__desc').textContent = el.topic_details;
     if (!el.expected_result)
@@ -15,11 +16,13 @@ const addCardToView = (...vidReq) => {
         .classList.add('d-none');
     cardEl.querySelector('.card__expected-res').textContent =
       el.expected_result;
+    cardEl.querySelector('.card__votes-count').textContent =
+      el.votes.ups - el.votes.downs;
     cardEl.querySelector('.card__status').textContent = el.status;
     cardEl.querySelector('.card__author').textContent = el.author_name;
     cardEl.querySelector('.card__date').textContent = new Date(
       el.submit_date
-    ).toLocaleDateString();
+    ).toLocaleString();
     cardEl.querySelector('.card__level').textContent = el.target_level;
 
     containerFragment.append(cardEl);
@@ -58,5 +61,25 @@ const formSubmitHandler = async (event) => {
   videoRequestFormEl.querySelector('button').disabled = false;
 };
 
+const makeVoteHandler = async (event) => {
+  if (!event.target.classList.contains('vote-btn')) return;
+  const cardId = event.target.closest('.card').id.slice(3, -3);
+  const voteType = event.target.dataset.voteType;
+  const response = await fetch('http://localhost:7777/video-request/vote', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: cardId,
+      vote_type: voteType,
+    }),
+  });
+  const data = await response.json();
+  event.target.parentElement.querySelector('.card__votes-count').textContent =
+    data.ups - data.downs;
+};
+
 fetchVideoRequest();
 videoRequestFormEl.addEventListener('submit', formSubmitHandler);
+listOfRequestsEl.addEventListener('click', makeVoteHandler);
