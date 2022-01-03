c5aadf9db862548bb92a1d9ccb00221101392ea1
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-01-03 15:47:04 +0200

    Added Sort Functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 const videoRequestFormEl = document.getElementById('video-request');
 const listOfRequestsEl = document.getElementById('list-of-requests');
 const videoRequestCardTemplate = document.getElementById('card-template');
+const sortElements = document.querySelectorAll('#filters button[id^=sort_by]');
 
-const addCardToView = (...vidReq) => {
-  console.log(vidReq);
+/**
+ * Clear Any cards in list if requests div
+ */
+const clearListOfRequestsEl = () => {
+  listOfRequestsEl.innerHTML = '';
+};
+
+/**
+ * Take array of video requests and append them in dom.
+ * @param  {Array} vidReq array of video requests.
+ */
+const addCardToView = (vidReq) => {
+  // console.log(vidReq);
   const containerFragment = document.createDocumentFragment();
   vidReq.forEach((el) => {
     const cardEl = document.importNode(videoRequestCardTemplate.content, true);
@@ -31,17 +43,33 @@ const addCardToView = (...vidReq) => {
   listOfRequestsEl.prepend(containerFragment);
 };
 
-const fetchVideoRequest = async () => {
+/**
+ * Fetch video requests from the server.
+ * @param  {...String} filter - filter or sort the request based on specific filter terms, available filters:
+ *  newFirst - topVotedFirst - all - planned - done
+ */
+const fetchVideoRequest = async (...filters) => {
   try {
     const response = await fetch('http://localhost:7777/video-request');
     if (!response.ok) throw new Error(response.statusText);
     const data = await response.json();
-    addCardToView(...data);
+    for (const filter of filters) {
+      if (filter === 'topVotedFirst')
+        data.sort(
+          (a, b) => b.votes.ups - b.votes.downs - (a.votes.ups - a.votes.downs)
+        );
+    }
+    console.log(data);
+    addCardToView(data);
   } catch (err) {
     console.log(err.message);
   }
 };
 
+/**
+ * Create new Video Request by submitting the form.
+ * @param {*} event
+ */
 const formSubmitHandler = async (event) => {
   event.preventDefault();
   const formData = new FormData(videoRequestFormEl);
@@ -49,11 +77,11 @@ const formSubmitHandler = async (event) => {
   try {
     const response = await fetch('http://localhost:7777/video-request', {
       method: 'POST',
-      body: formData,
+      body: formData
     });
     if (!response.ok) throw new Error(response.statusText);
     const data = await response.json();
-    addCardToView(data);
+    addCardToView([data]);
     videoRequestFormEl.reset();
   } catch (err) {
     console.log(err.message);
@@ -61,6 +89,10 @@ const formSubmitHandler = async (event) => {
   videoRequestFormEl.querySelector('button').disabled = false;
 };
 
+/**
+ * Make Vote Handler
+ * @param {*} event
+ */
 const makeVoteHandler = async (event) => {
   if (!event.target.classList.contains('vote-btn')) return;
   const cardId = event.target.closest('.card').id.slice(3, -3);
@@ -68,18 +100,35 @@ const makeVoteHandler = async (event) => {
   const response = await fetch('http://localhost:7777/video-request/vote', {
     method: 'PUT',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       id: cardId,
-      vote_type: voteType,
-    }),
+      vote_type: voteType
+    })
   });
   const data = await response.json();
   event.target.parentElement.querySelector('.card__votes-count').textContent =
     data.ups - data.downs;
 };
 
+/**
+ * Sort video requests based on sort type {newFirst - topVotedFirst}
+ * @param {*} event
+ */
+const sortResponsesHandler = (event) => {
+  const sortBy = event.target.dataset.type;
+  clearListOfRequestsEl();
+  fetchVideoRequest(sortBy);
+  event.target.classList.add('active');
+  const sibling =
+    event.target.previousElementSibling || event.target.nextElementSibling;
+  sibling.classList.remove('active');
+};
+
 fetchVideoRequest();
 videoRequestFormEl.addEventListener('submit', formSubmitHandler);
 listOfRequestsEl.addEventListener('click', makeVoteHandler);
+sortElements.forEach((element) => {
+  element.addEventListener('click', sortResponsesHandler);
+});
